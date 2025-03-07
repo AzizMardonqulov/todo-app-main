60068c11fc1a3dab6a1ff60d92f3ec5095b53a2f
AzizMardonqulov <=>
2025-03-07 17:13:17 +0500

    add checkbox click in  js

---
+++ mean.js
@@ -0,0 +1,5 @@
+let ResultCheckbox = document.getElementById("ResultCheckbox");
+let resultLable = document.getElementById("resultLable");
+ResultCheckbox.addEventListener('change' , function(){
+        resultLable.classList.toggle("done")
+})
